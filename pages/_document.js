f92711179eb3de1048bf0e9fff3906dd3686455c
Sean Anih <sean.anih@gmail.com>
2022-12-12 20:51:44 -0500

    completed Header and basic layout

--- pages/_document.js
+++ pages/_document.js
@@ -1,23 +1,19 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
+// APPLY FONTS AND STYLES TO <body> and entire app
 class MyDocument extends Document {
 	render() {
 		return (
 			<Html>
 				<Head>
-					{/* apply GoogleFonts to entire app */}
 					<link rel='preconnect' href='https://fonts.googleapis.com' />
+					<link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
 					<link
-						rel='preconnect'
-						href='https://fonts.gstatic.com'
-						crossOrigin='true'
-					/>
-					<link
-						href='https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,300;0,400;0,500;0,700;1,100;1,300;1,400;1,500;1,700&display=swap'
+						href='https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,300;0,400;0,500;0,700;1,100;1,300;1,400;1,500;1,700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;1,100;1,300;1,400;1,500;1,700&display=swap'
 						rel='stylesheet'
 					/>
 				</Head>
-				<body>
+				<body className='min-h-screen bg-slate-50 dark:bg-black dark:text-white'>
 					<Main />
 					<NextScript />
 				</body>
